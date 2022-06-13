b63040915ef4f4e71db103f803429cf30b6e6c02
ThanhRise <102305751+ThanhRise@users.noreply.github.com>
2022-06-13 22:10:25 +0700

    update _4EditAccount

---
+++ src/main/java/com/company/TestManager/Tests/_4EditAccount/EditAccountResponse.java
@@ -0,0 +1,2 @@
+package com.company.TestManager.Tests._4EditAccount;public class EditAccountResponse {
+}
---
+++ src/main/java/com/company/TestManager/Tests/_4EditAccount/UnitTests/EditAccountUnitTest10.java
@@ -0,0 +1,2 @@
+package com.company.TestManager.Tests._4EditAccount.UnitTests;public class EditAccountUnitTest10 {
+}
