a7ef10a848bf4ff09043ed2b4ffecfeb1360da50
Thanh Duc <do.thanh.duc.2002@gmail.com>
2022-07-04 11:44:38 +0700

    update Delete Comment

--- src/main/java/com/company/TestManager/Objects/Comment.java
+++ src/main/java/com/company/TestManager/Objects/Comment.java
@@ -1,6 +1,7 @@
 package com.company.TestManager.Objects;
 
 public class Comment {
+    public String comment_id;
     public String auction_id;
     public String user_id;
     public String content;
--- src/main/java/com/company/TestManager/TestAPIs/_14CreateComment/CreateCommentTest.java
+++ src/main/java/com/company/TestManager/TestAPIs/_14CreateComment/CreateCommentTest.java
@@ -6,8 +6,11 @@ import com.company.TestManager.TestAPIs._14CreateComment.UnitTests.CreateComment
 import com.company.TestManager.TestAPIs._14CreateComment.UnitTests.CreateCommentUnitTest2;
 import com.company.TestManager.TestAPIs._14CreateComment.UnitTests.CreateCommentUnitTest3;
 import com.company.TestManager.TestAPIs._14CreateComment.UnitTests.CreateCommentUnitTest4;
+import com.company.utils.Util;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreateCommentTest extends Test {
     public CreateCommentTest(BaseURL baseURL, String apiName) {
@@ -17,5 +20,15 @@ public class CreateCommentTest extends Test {
     @Override
     protected void initUnitTests() {
         this.unitTests.addAll(List.of(new CreateCommentUnitTest1(this),new CreateCommentUnitTest2(this), new CreateCommentUnitTest3(this), new CreateCommentUnitTest4(this)));
+    }
+    public static HashMap<String, String> generateDefaultParams() {
+
+        String content= Util.randomAlphabetString(201);
+        String comment_last_id="10";
+        return new HashMap<>(Map.of(
+                "content", content ,
+                "comment_last_id", comment_last_id
+
+        ));
     }
 }
